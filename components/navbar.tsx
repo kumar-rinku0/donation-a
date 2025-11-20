eb0b025f437ff923d5dbb6d6eabe45cfa397e8b7
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-11-20 15:45:40 +0530

    motion navigation added for mobile devices

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,34 +2,58 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import MotionNav from "@/components/motion-nav";
 
-const links = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About Us" },
-  { href: "/donate", label: "Donate" },
+const navLinks = [
+  { id: "/", name: "Home" },
+  { id: "/about", name: "About Us" },
+  { id: "/donate", name: "Donate" },
 ];
 
-const Navbar = () => {
+const MobileNav = ({ className }: { className: string }) => {
+  return (
+    <div className={className}>
+      <MotionNav navLinks={navLinks} />
+      <Link href={"/"} className="flex">
+        <span className="text-xl">Alpha Donate</span>
+      </Link>
+    </div>
+  );
+};
+
+const DesktopNav = ({ className }: { className: string }) => {
   const pathname = usePathname();
   return (
-    <div className="h-20 flex items-center justify-around px-8 bg-gray-800 text-white">
-      <h1>Alpha Donate</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          {links.map((link) => (
-            <li key={link.href}>
-              <Link
-                className={
-                  pathname === link.href ? "text-green-300" : "text-white"
-                }
-                href={link.href}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <div className={className}>
+      <Link href={"/"} className="flex">
+        <span className="text-xl">Alpha Donate</span>
+      </Link>
+      <div className="flex justify-center items-center gap-8">
+        {navLinks.map((link, idx) => {
+          return (
+            <Link
+              href={link.id}
+              key={idx}
+              title={link.name}
+              className={`capitalize ${
+                link.id === pathname ? "text-green-700" : "text-black"
+              }`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#e4e6f4] border-b border-gray-300">
+      <nav className="px-16 py-8">
+        <DesktopNav className="hidden md:flex md:justify-around md:items-center" />
+        <MobileNav className="flex md:hidden gap-4 items-center justify-center" />
       </nav>
     </div>
   );
